ea96637d0ad3e0549638e78c0fd7f1d876218ed7
thenikolayli <nikolay.li2008@gmail.com>
2026-02-20 02:01:48 -0800

    added polaroid

---
+++ web/src/lib/components/polaroid.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { photoUrl, captionUrl } = $props();
+</script>
+
+<section
+	class="flex h-88 w-62 flex-col overflow-hidden bg-white p-4 shadow-lg outline-1 outline-black/20"
+>
+	<img src={photoUrl} alt="" class="aspect-square w-full object-contain" />
+	<img src={captionUrl} alt="" class="min-h-0 w-full flex-1 object-fill" />
+</section>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,13 +1,20 @@
+<script>
+	import Polaroid from '$lib/components/polaroid.svelte';
+</script>
+
 <section class="main h-min-screen relative w-full">
 	<div class="spacer mx-auto h-48 w-1/2"></div>
-	<div class="mx-auto h-screen w-1/2">
+	<div class="relative mx-auto h-screen w-1/2">
 		<div class="flex items-center">
 			<hr class="w-12 rounded-l border-2" />
 			<h1 class="mx-2 font-[Crimson-Pro] text-5xl">Hello</h1>
 			<hr class="flex-1 rounded-r border-2" />
 		</div>
-		<div class="w-full pt-4 pl-4">
+		<div class="w-[70%] pt-4 pl-4">
 			<p class="font-[Figtree] text-2xl">I am Nikolay. I'm a student, musician, and developer.</p>
+		</div>
+		<div class="absolute top-4 right-8 rotate-10">
+			<Polaroid photoUrl="/yutaPFP.jpg" captionUrl="/yutaPFP.jpg" />
 		</div>
 	</div>
 	<div class="mx-auto h-screen w-1/2">
@@ -32,9 +39,9 @@
 	</div>
 </section>
 
-<style>
+<!-- <style>
 	.main {
 		background-image: url('/bedge-grunge.png');
 		background-color: var(--color-DarkCoffee);
 	}
-</style>
+</style> -->

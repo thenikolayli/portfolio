472c3c0a54b25ed79e369436618c4cc4f1e3b7f0
thenikolayli <nikolay.li2008@gmail.com>
2026-02-21 02:58:27 -0800

    added wiggle

--- web/src/lib/components/polaroid.svelte
+++ web/src/lib/components/polaroid.svelte
@@ -1,8 +1,46 @@
 <script>
-	let { photoUrl, captionUrl } = $props();
+	let { photoUrl, captionUrl, disableAnimation = false } = $props();
+	import gsap from 'gsap';
+	import { onMount } from 'svelte';
+
+	let polaroid;
+	let wiggle;
+
+	onMount(() => {
+		wiggle = gsap.to(polaroid, {
+			rotation: 3,
+			duration: 0.3,
+			ease: 'power1.inOut',
+			yoyo: true,
+			repeat: -1,
+			paused: true
+		});
+	});
+
+	const onMouseEnter = () => {
+		gsap.to(polaroid, {
+			scale: 1.05,
+			duration: 0.3,
+			ease: 'power1.out'
+		});
+		wiggle.play();
+	};
+
+	const onMouseLeave = () => {
+		gsap.to(polaroid, {
+			scale: 1,
+			duration: 0.3,
+			ease: 'power1.out'
+		});
+		wiggle.pause();
+	};
 </script>
 
 <section
+	bind:this={polaroid}
+	onmouseenter={() => !disableAnimation && onMouseEnter()}
+	onmouseleave={() => !disableAnimation && onMouseLeave()}
+	role="note"
 	class="flex h-88 w-62 flex-col overflow-hidden bg-white p-4 shadow-lg outline-1 outline-black/20"
 >
 	<img src={photoUrl} alt="" class="aspect-square w-full object-contain" />
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,42 +1,80 @@
 <script>
-	import Polaroid from '$lib/components/polaroid.svelte';
+	import Polaroid from '$lib/components/Polaroid.svelte';
+	import { onMount } from 'svelte';
+
+	let hobbyFocused = $state(1); // integer 1 <= n <= 3
+	let hobbies = {};
+	onMount(() => {
+		document.title = 'Nikolay Li';
+	});
 </script>
 
 <section class="main h-min-screen relative w-full">
 	<div class="spacer mx-auto h-48 w-1/2"></div>
-	<div class="relative mx-auto h-screen w-1/2">
+	<section class="relative mx-auto h-screen w-1/2">
 		<div class="flex items-center">
 			<hr class="w-12 rounded-l border-2" />
 			<h1 class="mx-2 font-[Crimson-Pro] text-5xl">Hello</h1>
 			<hr class="flex-1 rounded-r border-2" />
 		</div>
+
 		<div class="w-[70%] pt-4 pl-4">
 			<p class="font-[Figtree] text-2xl">I am Nikolay. I'm a student, musician, and developer.</p>
 		</div>
+
 		<div class="absolute top-4 right-8 rotate-10">
 			<Polaroid photoUrl="/yutaPFP.jpg" captionUrl="/yutaPFP.jpg" />
 		</div>
-	</div>
-	<div class="mx-auto h-screen w-1/2">
+	</section>
+	<section class="relative mx-auto h-screen w-1/2">
 		<div class="flex items-center">
 			<hr class="w-12 border-2" />
 			<h1 class="mx-2 font-[Crimson-Pro] text-5xl">Hello</h1>
 			<hr class="flex-1 border-2" />
 		</div>
+
 		<div class="w-full pt-4 pl-4">
 			<p class="font-[Figtree] text-2xl">I am Nikolay. I'm a student, musician, and developer.</p>
 		</div>
-	</div>
-	<div class="mx-auto h-screen w-1/2">
+
+		<div class="wrapper absolute top-4 right-0 flex w-1/2 items-center justify-center">
+			<div
+				class="-rotate-10"
+				onmouseenter={() => (hobbyFocus = 1)}
+				role="note"
+				bind:this={hobbies[0]}
+			>
+				<Polaroid photoUrl="/yutaPFP.jpg" captionUrl="/yutaPFP.jpg" disableAnimation={true} />
+			</div>
+			<div
+				class="rotate-0"
+				onmouseenter={() => (hobbyFocus = 2)}
+				role="note"
+				bind:this={hobbies[1]}
+			>
+				<Polaroid photoUrl="/yutaPFP.jpg" captionUrl="/yutaPFP.jpg" disableAnimation={true} />
+			</div>
+			<div
+				class="rotate-10"
+				onmouseenter={() => (hobbyFocus = 3)}
+				role="note"
+				bind:this={hobbies[2]}
+			>
+				<Polaroid photoUrl="/yutaPFP.jpg" captionUrl="/yutaPFP.jpg" disableAnimation={true} />
+			</div>
+		</div>
+	</section>
+	<section class="mx-auto h-screen w-1/2">
 		<div class="flex items-center">
 			<hr class="w-12 border-2" />
 			<h1 class="mx-2 font-[Crimson-Pro] text-5xl">Hello</h1>
 			<hr class="flex-1 border-2" />
 		</div>
+
 		<div class="w-full pt-4 pl-4">
 			<p class="font-[Figtree] text-2xl">I am Nikolay. I'm a student, musician, and developer.</p>
 		</div>
-	</div>
+	</section>
 </section>
 
 <!-- <style>
